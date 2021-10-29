75c2def59bae0e1e27e926eda47d6d49ec8b60b9
Neykruatick <neykuratick@yahoo.com>
2021-10-29 13:01:04 +0300

    installer + modified cheatsheet

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh  
+
+exec cp -r .config/gxkb ~/.config/ &
+cp -r .config/htop ~/.config/ &
+cp -r .config/qtile ~/.config/ &
+cp xlayout.sh ~ &
+cp brightness.sh ~ &
+cp fixf.sh ~ &
+cp logout.sh ~ &
+cp .vimrc ~ &
+cp .zshrc ~ 
