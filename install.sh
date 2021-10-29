987db092baefb6b76c51798995d7b7fdb9d4a290
Neykruatick <neykuratick@yahoo.com>
2021-10-29 15:32:11 +0300

    refactor

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/sh  
 
-exec chsh -s $(which zsh) &
-exec cp -r .config/gxkb ~/.config/ &
-cp -r .config/htop ~/.config/ &
+#exec sudo chsh -s $(which zsh) &
+exec cp -r .config/htop ~/.config/ &
 cp -r .config/qtile ~/.config/ &
-cp xlayout.sh ~ &
-cp kblayout.sh ~ &
-cp brightness.sh ~ &
-cp fixf.sh ~ &
-cp logout.sh ~ &
+cp shell_scripts/xlayout.sh ~ &
+cp shell_scripts/kblayout.sh ~ &
+cp shell_scripts/brightness.sh ~ &
+cp shell_scripts/fixf.sh ~ &
+cp shell_scripts/logout.sh ~ &
 cp .vimrc ~ &
 cp .xinitrc ~ &
 cp .zshrc ~ 
